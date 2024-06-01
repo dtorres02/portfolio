2fc54d60fe742042ae9aeae6159d43c0b95ae167
Derik Torres <deriktorres33@gmail.com>
2024-06-01 14:03:15 -0400

    Updated header and nav bar with hamburger div

--- professional-portfolio/src/app/components/Header.tsx
+++ professional-portfolio/src/app/components/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -6,11 +7,55 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import { ListItemButton, ListItemIcon, ListItem, ListItemText } from '@mui/material';
+
+// Icon Imports from mui
+import SchoolIcon from '@mui/icons-material/school';
+import PersonIcon from '@mui/icons-material/Person';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import PublicIcon from '@mui/icons-material/Public';
+import HomeIcon from '@mui/icons-material/Home';
+
 
 export default function ButtonAppBar() {
+
+    // Drawer State Controller
+    const [open, setOpen] = React.useState(false);
+
+    // Drawer Toggle Function
+    const toggleDrawer = (newOpen: boolean) => () => {
+      setOpen(newOpen)
+    };
+
+    // Drawer Constant
+    const DrawerList = (
+      <Box sx = {{width: 250}} role = "presentation" onClick = {toggleDrawer(false)}>
+        <List>
+          {['Home','About Me', 'Projects', 'Education', 'Social Media'].map((text, index) =>
+            <ListItem key = {text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index === 0 && <HomeIcon/>}
+                  {index === 1 && <PersonIcon/>}
+                  {index === 2 && <AccountTreeIcon/>}
+                  {index === 3 && <SchoolIcon/>}
+                  {index === 4 && <PublicIcon/>}
+                </ListItemIcon>
+                <ListItemText primary = {text}/>
+              </ListItemButton>
+            </ListItem>
+          )}
+        </List>
+      </Box>
+    );  
+
+
+
   return (
     <Box sx={{ maxWidth: 1}}>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton
             size="large"
@@ -18,9 +63,13 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick = {toggleDrawer(true)}
           >
             <MenuIcon />
           </IconButton>
+          <Drawer open = {open} onClose = {toggleDrawer(false)}>
+            {DrawerList}
+          </Drawer>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             
           </Typography>
--- professional-portfolio/src/app/components/pages/MainPage.tsx
+++ professional-portfolio/src/app/components/pages/MainPage.tsx
@@ -5,8 +5,16 @@ export default function MainPage() {
     return(
         <>
             <Header/>
+            <HomePage/>
         </>
         
     );
 }
 
+export function HomePage() {
+    return(
+        <div>
+            Hello! Welcome to my Professional Website!
+        </div>
+    )
+};
